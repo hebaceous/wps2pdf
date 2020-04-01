c817bb29d8905b1989f542d68471b4af50e05f47
hebaceous <hebaceous@gmail.com>
2020-04-01 13:46:40 +0800

    try...finally

--- src/main/kotlin/me/hebaceous/wps2pdf/WPS.kt
+++ src/main/kotlin/me/hebaceous/wps2pdf/WPS.kt
@@ -4,22 +4,25 @@ import com.jacob.activeX.ActiveXComponent
 import com.jacob.com.Dispatch
 
 import java.io.File
-import java.io.IOException
 import java.nio.file.Files
 
 object WPS {
-
-    @Throws(IOException::class)
-    fun word2pdf(word: File): File {
-        val pdf = Files.createTempFile(null, ".pdf").toFile()
-        val app = ActiveXComponent("Word.Application")
-        app.setProperty("Visible", false)
-        val docs = app.getProperty("Documents").toDispatch()
-        val doc = Dispatch.call(docs, "Open", word.absolutePath, false, true).toDispatch()
+  fun word2pdf(word: File): File {
+    val pdf = Files.createTempFile(null, ".pdf").toFile()
+    val app = ActiveXComponent("Word.Application")
+    try {
+      app.setProperty("Visible", false)
+      val docs = app.getProperty("Documents").toDispatch()
+      val doc = Dispatch.call(docs, "Open", word.absolutePath, false, true).toDispatch()
+      try {
         Dispatch.call(doc, "ExportAsFixedFormat", pdf.absolutePath, 17)
+      } finally {
         Dispatch.call(doc, "Close", false)
-        app.invoke("Quit", 0)
-        return pdf
+      }
+    } finally {
+      app.invoke("Quit", 0)
     }
+    return pdf
+  }
 
 }
